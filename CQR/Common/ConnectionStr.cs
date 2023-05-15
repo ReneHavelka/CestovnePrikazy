70b526c2c2c702c1ea99de644db1799e8a73910e
Rene Havelka <havelka@outlook.sk>
2023-05-15 10:11:56 +0200

    Connection - several databases

--- CQR/Common/ConnectionStr.cs
+++ CQR/Common/ConnectionStr.cs
@@ -1,14 +1,16 @@
-﻿namespace CestovnePrikazy.CQR.Common
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CestovnePrikazy.CQR.Common
 {
     internal class ConnectionStr
     {
         internal string ConnectionString { get; }
 
-        internal ConnectionStr()
+        internal ConnectionStr(string dtBase = @"C:\Users\RH\source\repos\CestovnePrikazy\TravelExpenses.mdf")
         {
-            ConnectionString =
-              @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\RH\source\repos\CestovnePrikazy\TravelExpenses.mdf;
-              Integrated Security=True";
+            ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Integrated Security=True;";
+            ConnectionString += "AttachDbFilename=" + dtBase;
         }
     }
 }
